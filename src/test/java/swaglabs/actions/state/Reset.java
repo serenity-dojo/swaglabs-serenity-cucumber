60b84a39bba20a7915ff99663cbdd029bf650475
John Ferguson Smart <john.smart@wakaleo.com>
2021-11-05 09:47:06 +0000

    Added extra conditional waits to cater for slow browsers

--- src/test/java/swaglabs/actions/state/Reset.java
+++ src/test/java/swaglabs/actions/state/Reset.java
@@ -4,14 +4,22 @@ import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Browser;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.targets.Target;
 import net.serenitybdd.screenplay.ui.Button;
 import net.serenitybdd.screenplay.ui.Link;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isPresent;
 
 public class Reset {
+    private static final Target RESET_APP_STATE = Link.called("Reset App State");
+    private static final Target MENU_BUTTON = Button.called("Open Menu");
+
     public static Performable applicationState() {
         return Task.where(
-                Click.on(Button.called("Open Menu")),
-                Click.on(Link.called("Reset App State")),
+                Click.on(MENU_BUTTON),
+                WaitUntil.the(RESET_APP_STATE, isPresent()),
+                Click.on(RESET_APP_STATE),
                 Browser.refreshPage()
         ).withNoReporting();
     }
